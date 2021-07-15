1298efc191680f2b19b2fb78d658cf826f61ecbd
Marshall Danel <marshalldanel@gmail.com>
2021-07-14 22:57:38 -0700

    Add Axios, api data file, fetch data for state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
+import { useState, useEffect, useCallback } from 'react';
+const axios = require('axios');
+
 function App() {
+  const [planetFacts, setPlanetFacts] = useState([]);
+
+  const fetchFacts = useCallback(async () => {
+    const response = await axios.get('data.json');
+    setPlanetFacts(response.data);
+  }, []);
+
+  useEffect(() => {
+    fetchFacts();
+  }, [fetchFacts]);
   return (
     <>
       <p>Facts go here.</p>
