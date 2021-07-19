e869a46c53d06eedbd89341e1788b6df849e4c5a
Marshall Danel <marshalldanel@gmail.com>
2021-07-19 15:59:47 -0700

    Add mobile button styles

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,46 @@
 import React from 'react';
+import styled from 'styled-components';
 
-export default function Button({ view, setCurrentView }) {
-  return <button onClick={() => setCurrentView(view)}>{view}</button>;
+const StyledButton = styled.button`
+  height: 5rem;
+  flex-grow: 1;
+  border: none;
+  background: none;
+  color: #fff;
+  text-transform: uppercase;
+  line-height: 1rem;
+  font-size: 0.9rem;
+  letter-spacing: 0.193rem;
+  font-family: Antonio;
+  font-weight: 700;
+  position: relative;
+
+  ${(props) =>
+    props.view === props.currentView
+      ? `
+        :after {
+          content: "";
+          background: ${props.theme[props.name.toLowerCase()]};
+          position: absolute;
+          bottom: 0;
+          left: 25%;
+          height: .4rem;
+          width: 50%;
+        };`
+      : `
+        opacity: .5;
+        `};
+`;
+
+export default function Button({ view, setCurrentView, currentView, name }) {
+  return (
+    <StyledButton
+      name={name}
+      onClick={() => setCurrentView(view)}
+      view={view}
+      currentView={currentView}
+    >
+      {view}
+    </StyledButton>
+  );
 }
--- src/components/PlanetPage.js
+++ src/components/PlanetPage.js
@@ -1,7 +1,14 @@
 import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import styled from 'styled-components';
 import Planet from './Planet';
 import Button from './Button';
+
+const StyledButtons = styled.div`
+  display: flex;
+  flex-flow: row;
+  border-bottom: 0.1rem solid rgba(255, 255, 255, 0.2);
+`;
 
 export default function PlanetPage({ homePlanet }) {
   let { state } = useLocation();
@@ -14,9 +21,17 @@ export default function PlanetPage({ homePlanet }) {
 
   return (
     <div>
-      {views.map((view) => (
-        <Button key={view} view={view} setCurrentView={setCurrentView}></Button>
-      ))}
+      <StyledButtons>
+        {views.map((view) => (
+          <Button
+            key={view}
+            currentView={currentView}
+            view={view}
+            setCurrentView={setCurrentView}
+            name={planet.name}
+          ></Button>
+        ))}
+      </StyledButtons>
       <Planet planet={planet} currentView={currentView} />
     </div>
   );
