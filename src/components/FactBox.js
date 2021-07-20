7a6352a0e9421b6cc4703090eba39e16c5de16cc
Marshall Danel <marshalldanel@gmail.com>
2021-07-20 16:39:31 -0700

    Change to styled components macro for debuging

--- src/components/FactBox.js
+++ src/components/FactBox.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled from 'styled-components/macro';
 import { mediaQueries } from '../styles/mediaQueries';
 
 const StyledBox = styled.div`
