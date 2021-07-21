1ea56f6309c8ffa8467221e178f4a60785a98875
Marshall Danel <marshalldanel@gmail.com>
2021-07-21 10:57:16 -0700

    Add react-icons. mobile description styles

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,12 +1,45 @@
 import React from 'react';
+import styled from 'styled-components';
+import { FaExternalLinkAlt } from 'react-icons/fa';
+
+const StyledContainer = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+  margin: 2.8rem 2.4rem;
+`;
+
+const StyledInfo = styled.p`
+  font-size: 1.1rem;
+  line-height: 2.2rem;
+  text-align: center;
+  margin: 1.6rem 0 3.2rem 0;
+`;
+
+const StyledLink = styled.a`
+  font-size: 1.2rem;
+  line-height: 2.5rem;
+  font-family: Spartan;
+  font-weight: normal;
+  color: rgba(255, 255, 255, 0.5);
+  text-decoration: none;
+`;
+
+const StyledFaIcon = styled(FaExternalLinkAlt)`
+  margin-left: 0.5rem;
+  font-size: 0.9rem;
+  color: rgba(255, 255, 255, 0.5);
+`;
 
 export default function Description({ name, info }) {
-  console.log('info :>> ', info);
   return (
-    <>
+    <StyledContainer>
       <h2>{name}</h2>
-      <p>{info.content}</p>
-      <h4>{info.source}</h4>
-    </>
+      <StyledInfo>{info.content}</StyledInfo>
+      <StyledLink href={info.source}>
+        Source: <u>Wikipedia</u>
+        <StyledFaIcon />
+      </StyledLink>
+    </StyledContainer>
   );
 }
